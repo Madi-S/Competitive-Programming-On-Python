92c8ef95466a4920f7cc2e85c4070efb3e5eb33a
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-24 22:21:30 +0600

    reverse a sequence added

---
+++ Algorithms/Reverse_Sequence.py
@@ -0,0 +1,13 @@
+
+
+def reverse(seq):
+    # Take simmetric values and swap them
+
+    for i in range(len(seq) // 2):
+        simmetric = len(seq) - 1 - i    # or -i - 1
+        seq[i], seq[simmetric] = seq[simmetric], seq[i]
+
+    print(seq)
+
+
+print(reverse([3, 4, 5, 6, 7, 8, 9, 10, 11]))
