a2709d037b048c235bbe6632ae778b20a04e89f5
Madi <72963376+Madi-S@users.noreply.github.com>
2020-11-30 13:12:52 +0600

    solution for neznaika fixed

--- Acmp Tasks/Незнайка.py
+++ Acmp Tasks/Незнайка.py
@@ -6,19 +6,20 @@ From https://acmp.ru/index.asp?main=task&id_task=922
 def count_time(t1, t2, s1, s2, s):
     if s2 >= s1:
         return 'NO'
-    time_count = 0
-    distance_traveled = 0
+    time_count = distance_traveled = 0
     while distance_traveled < s:
+        print(time_count, distance_traveled)
         left = s - distance_traveled
         if left <= s1:
+            print('here', left)
             distance_traveled += left
-            time_count += t1
+            time_count += (left / s1) * t1
             break
 
         distance_traveled += s1
         time_count += t1
 
-        distance_traveled -= 1
+        distance_traveled -= s2
         time_count += t2
 
     return time_count
