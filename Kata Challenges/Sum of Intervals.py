d19f1e6e738c22a260fce2e3f7551d21b75dc38f
Madi <72963376+Madi-S@users.noreply.github.com>
2021-02-23 13:23:01 +0600

    sum of intervals kata added

---
+++ Kata Challenges/Sum of Intervals.py
@@ -0,0 +1,10 @@
+
+
+def sum_of_intervals(intervals):
+    s, top = 0, float('-inf')
+    for a,b in sorted(intervals):
+        if top < a:
+            top = a
+        if top < b:
+            s, top = s + b - top, b
+    return s
