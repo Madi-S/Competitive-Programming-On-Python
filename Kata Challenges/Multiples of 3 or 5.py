a17cd8605aad746d11101ebfab0c2e2b9f6fe300
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-22 12:03:03 +0600

    Sum of Digits / Digital Root kata solution added

--- Kata Challenges/Multiples of 3 or 5.py
+++ Kata Challenges/Multiples of 3 or 5.py
@@ -1,8 +1,6 @@
 def solution(num):
-	if number < 0:
-		return 0
-	res = []
-	for i in range(3, num+1):
+	res = 0
+	for i in range(3, num):
 		if i % 3 == 0 or i % 5 == 0:
-			res.append(i)
+			res += i
 	return res
---
+++ Kata Challenges/Sum of Digits Digital Root.py
@@ -0,0 +1,2 @@
+def digital_root(n):
+	return n if n < 10 else n%9 or 9
