2c362fcd5f381d5cbcb0b754b5ec2c84385b944b
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-24 09:54:36 +0600

    Stop gninnipS My sdroW! kata solution added

---
+++ Kata Challenges/Stop gninnipS My sdroW!.py
@@ -0,0 +1,11 @@
+
+def spin_words(sentence):
+	return ' '.join(list(map(lambda a: a[::-1] if len(a) >= 5 else a, sentence.split())))
+	
+
+
+
+
+spin_words("Hey fellow warriors")
+spin_words("This is a test") 
+spin_words("This is another test")
