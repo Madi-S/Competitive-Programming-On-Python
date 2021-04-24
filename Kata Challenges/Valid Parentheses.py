4cfdecac023bbbfc86cc4b39f32c1fff6c4b6616
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-24 14:17:01 +0600

    Valid Parentheses kata solution added

---
+++ Kata Challenges/Valid Parentheses.py
@@ -0,0 +1,25 @@
+
+def valid_parentheses(s):
+	if s.startswith(')'):
+		return False
+
+	s = list(filter(lambda p: p in '()', s))
+	
+	if len(s) % 2 != 0:
+		return False
+
+	buf = []
+	for p in s:
+		if p == '(':
+			buf.append(s)
+		else:
+			try:
+				buf.pop()
+			except Exception as e:
+				return False
+	if buf:
+		return False
+	return True
+
+
+print(valid_parentheses('()()'))
