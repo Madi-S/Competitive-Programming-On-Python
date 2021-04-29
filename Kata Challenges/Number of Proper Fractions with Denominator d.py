d9c5035635cd258f11eee0f39726c0558e285e26
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-29 15:23:17 +0600

    Number of Proper Fractions with Denominator d kata solution added

---
+++ Kata Challenges/Number of Proper Fractions with Denominator d.py
@@ -0,0 +1,26 @@
+from math import sqrt
+from functools import reduce
+
+
+def _get_primes(n):
+	pr = set()
+	i = 2
+	while i * i <= n:
+		if n % i == 0:
+			pr.add(i)
+			n //= i
+		else:
+			i += 1
+	return pr
+
+
+def proper_fractions(n):
+	primes = _get_primes(n)
+	res = n
+	for pr in primes:
+		res *= (1 - (1 / pr)) 
+
+	return int(res)
+
+
+print(proper_fractions(100))
