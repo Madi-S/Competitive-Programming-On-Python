75535fcac8e9a1eab431a1dfcd2937aee0c3ca8c
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-30 22:31:44 +0600

    fizzubzz kata solution added

---
+++ Kata Challenges/Sum of pairs.py
@@ -0,0 +1,31 @@
+def sum_pairs(ints, s):
+	if sum(ints) < s:
+		return None
+
+
+	i, j, k = 0, 1, 2
+
+		
+	while k <= len(ints):
+		int1 = ints[i]
+		int2 = ints[j]
+
+		if int1 + int2 == s:
+			print([int1, int2])
+			return [int1, int2]
+			break
+
+		i += 1
+		j += 1
+		print(i, j)
+
+		if j == len(ints):
+			print('incr', k)
+			i = 0
+			j = k
+			k += 1
+
+	print('nothing')
+	return None
+
+sum_pairs([4, -2, 3, 3, 4], 8)
